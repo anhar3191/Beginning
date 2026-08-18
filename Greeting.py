8aff2b7ce5fdb3a0eff46a0f580dce1f4e020231
Anhar Hamza <anharhmzt79@gmail.com>
2026-08-18 18:21:53 +0200

    Add files via upload

---
+++ Greeting.py
@@ -0,0 +1,3 @@
+name =input("what is your name?")
+print("Hello", name)
+print("welcome to the python!")
---
+++ age.py
@@ -0,0 +1,3 @@
+birth_year=int(input("Enter your birth year:"))
+age =2026-2001
+print("your age is ",age)
